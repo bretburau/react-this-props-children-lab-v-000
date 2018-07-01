9bfd3dc11a6a6251b5d5642cde86cd42d7890788
bretburau <github email address>
2018-07-01 04:35:05 -0500

    Done.

--- src/components/Invitation.js
+++ src/components/Invitation.js
@@ -1,1 +1,18 @@
 // Code Invitation Component Here
+import React from 'react';
+
+
+
+export default class Invitation extends React.Component {
+  render() {
+    const childrenWithProps = React.Children.map(this.props.children, child => {
+      return React.cloneElement(child)
+    })
+    return(
+      <div>
+        <h1>You've been invited!</h1>
+        {childrenWithProps}
+      </div>
+    )
+  }
+}
--- src/components/ThemedDecorations.js
+++ src/components/ThemedDecorations.js
@@ -1,1 +1,19 @@
 // Code ThemedDecoration Component Here
+import React from 'react';
+
+export default class ThemedParty extends React.Component {
+  render() {
+    const childrenWithProps = React.Children.map(this.props.children, child => {
+        return(
+          <div className={this.props.theme}>
+            {child}
+          </div>
+        )
+    })
+    return(
+      <div>
+        {childrenWithProps}
+      </div>
+    )
+  }
+}
